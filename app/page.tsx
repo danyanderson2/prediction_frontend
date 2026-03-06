f3b4d4699246aad4c8d6e1700c769872dc25712b
Dany Anderson <danyanderson2222@gmail.com>
2026-03-06 17:36:19 +0000

    backend

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,6 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Toaster position="top-right" />
-      
       {/* Header */}
       <header className="bg-white shadow-material-16 border-b-8 border-blue-600">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
